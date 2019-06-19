fe3bd9449ed58787340b014b1d6154e0096488ce
Owen Edwards <omedwards@outlook.com>
2019-06-19 19:38:54 -0400

    updating api

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
-const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
-
 var isGameOver;
 var player;
-var playerImage;
+var playerImage2;
 var enemy;
 var enemyImage;
 var backgroundImage;
-let url;
+var poImage;
 
-var Pokedex = require('pokedex-promise-v2');
-var P = new Pokedex();
 
-P.getPokemonByName("butterfree")
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log('There was an ERROR: ', error);
+let pokemonSearchButton = document.getElementById("pokeForm");
+pokemonSearchButton.addEventListener('submit', pokeSubmit);
+let pokemonImage = document.getElementById("myPokemon");
+
+
+function pokeSubmit(event) {
+  event.preventDefault();
+  document.getElementById("submitButton").className = "ui-btn";
+  // API accepts lowercase only
+  var param = document.getElementById("pokeInput").value.toLowerCase();
+  var pokeURL = "http://pokeapi.co/api/v2/pokemon/" + param;
+
+  $.ajax({
+    type: "GET",
+    crossDomain: true,
+    url: pokeURL,
+    dataType: "json"
+  }).done(function (data) {
+    console.log(data); // Able to pull pokemon json this line is important
+    let poImage = data.sprites.front_default;
+    console.log(poImage);
+    let playerImage = poImage;
+    document.getElementById("myPokemon").attribute('background-image', url(poImage));
+
+
+
+    return playerImage;
   });
+
+}
+console.log(playerImage);
 
 
 function preload() {
-  playerImage = loadImage("gliscor.png");
-  enemyImage = loadImage("blackferaligatr.png");
-  backgroundImage = loadImage("https://images.unsplash.com/photo-1526392587392-d1627b6c134a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60");
+  let playerImage2 = loadImage(playerImage);
+  let enemyImage = loadImage("blackferaligatr.png");
+  let backgroundImage = loadImage("https://images.unsplash.com/photo-1526392587392-d1627b6c134a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60");
+
 }
+
+function setup() {
+  isGameOver = false;
+  createCanvas(600, 600); // creates canvas on screen
+  player = createSprite(width / 2, height - (playerImage2.height / 2), 0, 0); // Sets initial position of players pokemon
+  player.drawImage(playerImage2); // sets where the player is to the chosen pokemon from the api
+  enemy = createSprite(width / 2, 0, 0, 0); // creates an instance of an enemy at the top of screen falling downwards
+  enemy.addImage(enemyImage); // sets where that enemy was instanced to a random pokemon
+  enemy.rotationSpeed = 4.0; // speed at which the random pokemon is spinning
+}
+
+
 
 // TODO: Set up function to pull the list of pokemon from the json
 // TODO: Set up constant variable to be the list of all pokemon
@@ -32,18 +65,6 @@ function preload() {
 
 
 
-
-
-
-function setup() {
-  isGameOver = false;
-  createCanvas(600, 600); // creates canvas on screen
-  player = createSprite(width / 2, height - (playerImage.height / 2), 0, 0); // Sets initial position of players pokemon
-  player.addImage(playerImage); // sets where the player is to the chosen pokemon from the api
-  enemy = createSprite(width / 2, 0, 0, 0); // creates an instance of an enemy at the top of screen falling downwards
-  enemy.addImage(enemyImage); // sets where that enemy was instanced to a random pokemon
-  enemy.rotationSpeed = 4.0; // speed at which the random pokemon is spinning
-}
 
 function draw() {
   if (isGameOver) {
@@ -53,11 +74,11 @@ function draw() {
       isGameOver = true;
     }
     background(backgroundImage);
-    if (keyDown(RIGHT_ARROW) && player.position.x < (width - (playerImage.width / 2))) {
-      player.position.x += 2;
+    if (keyDown(RIGHT_ARROW) && player.position.x < (width - (playerImage2.width / 2))) {
+      player.position.x += 4;
     }
-    if (keyDown(LEFT_ARROW) && player.position.x > (playerImage.width / 2)) {
-      player.position.x -= 2;
+    if (keyDown(LEFT_ARROW) && player.position.x > (playerImage2.width / 2)) {
+      player.position.x -= 4;
     }
     enemy.position.y = enemy.position.y + 8;
     if (enemy.position.y > height) {
@@ -77,11 +98,6 @@ function gameOver() {
   text("Click anywhere to try again", width / 2, 3 * height / 4); // Instructions of how to resume playing after losing
 }
 
-// Mouse clicked function 
-function mouseClicked() {
-  isGameOver = false;
-  player.position.x = width / 2;
-  player.position.y = height - (playerImage.height / 2);
-  enemy.position.x = width / 2;
-  enemy.position.y = 0;
-}
+
+
+
